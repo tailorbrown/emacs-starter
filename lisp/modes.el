6cf8ea0808d76e13128e9fa1a1ad4ada8d4a8c17
Dylan W. Schwilk <dylan@schwilk.org>
2014-04-22 16:50:57 -0500

    Clean modes.el
    From working:
    - Remove extra comments
    - Gather all commint-mode customizations together
    - delete a few redundant variable setqs
    
    Conflicts:
    	lisp/modes.el
    
    Resolved conflicts, added c/c++ support in the process

--- lisp/modes.el
+++ lisp/modes.el
@@ -3,8 +3,8 @@
 ;; modes.el configuration file
 ;; author: Dylan Schwilk
 ;;
-;; Provides hooks and customizations for various modes (text, LaTeX and bibtex,
-;; html, C, C++, python, ess (R and julia).
+;; Provides hooks and customizations for various modes (text, markdown, LaTeX
+;; and bibtex, html, C, C++, python, ess (R and julia).
 ;;
 ;; All org-mode customizations are in ~/.emacs.d/lisp/org-mode-setup.el
 ;;;;---------------------------------------------------------------------------
@@ -88,6 +88,70 @@
 ;; End setup lisp-mode --------------------------------------------------------
 
 ;;;----------------------------------------------------------------------------
+;; Setup C, C++ mode
+(autoload 'c++-mode  "cc-mode" "C++ Editing Mode" t)
+(autoload 'c-mode    "cc-mode" "C Editing Mode" t)
+(autoload 'c-mode-common-hook "cc-mode" "C Mode Hooks" t)
+(autoload 'c-add-style "cc-mode" "Add coding style" t)
+
+;; Associate extensions with c modes
+(add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
+
+;; Create my own c/cpp coding style
+;; No space before { and function sig indents 4 if argument overflow
+(setq dws-c-style
+      '((c-auto-newline                 . nil)
+        (c-basic-offset                 . 4)
+        (c-comment-only-line-offset     . 0)
+        (c-echo-syntactic-information-p . nil)
+        (c-hungry-delete-key            . t)
+        (c-tab-always-indent            . t)
+        (c-toggle-hungry-state          . t)
+        (c-hanging-braces-alist         . ((substatement-open after)
+                                          (brace-list-open)))
+        (c-offsets-alist                . ((arglist-close . c-lineup-arglist)
+                                           (case-label . 4)
+                                           (substatement-open . 0)
+                                           (block-open . 0) ; no space before {
+                                           (knr-argdecl-intro . -)))
+        (c-hanging-colons-alist         . ((member-init-intro before)
+                                           (inher-intro)
+                                           (case-label after)
+                                           (label after)
+                                           (access-label after)))
+        (c-cleanup-list                 . (scope-operator
+                                           empty-defun-braces
+                                           defun-close-semi))))
+        
+;; Construct a hook to be called when entering C mode
+(defun lconfig-c-mode ()
+  (progn (define-key c-mode-base-map "\C-m" 'newline-and-indent)
+         (define-key c-mode-base-map [f4] 'speedbar-get-focus)
+         (define-key c-mode-base-map [f5] 'next-error)
+         (define-key c-mode-base-map [f6] 'run-program)
+         (define-key c-mode-base-map [f8] 'compile)
+         (define-key c-mode-base-map [f9] 'insert-breakpoint)
+         (define-key c-mode-base-map [f10] 'step-over)
+         (define-key c-mode-base-map [f11] 'step-into)
+         (c-add-style "Dylan's Coding Style" dws-c-style t)))
+(add-hook 'c-mode-common-hook 'lconfig-c-mode)
+;; End setup c mode -----------------------------------------------------------
+
+;;;----------------------------------------------------------------------------
+;; Setup comint mode (command-line-interpreter) for R, julia, python, shell etc
+(setq ansi-color-for-comint-mode t) ;; show ansi terminal colors
+(setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+(setq comint-move-point-for-output t)
+
+;;;----------------------------------------------------------------------------
+;; Setup Shell mode
+
+; nothing to do. All set up in comint mode, above.
+
+;; End setup shell-mode -------------------------------------------------------
+
+;;;----------------------------------------------------------------------------
 ;; python mode
 
 ;; nothing to do should work
@@ -95,25 +159,13 @@
 ;; End setup python-mode-------------------------------------------------------
 
 ;;;----------------------------------------------------------------------------
-;; Setup R mode and and define the shift-enter function
-(require 'ess-site)
+;; Setup R and julia mode using ESS
+(require 'ess-site) 
 (setq ess-ask-for-ess-directory nil)
-(setq ess-local-process-name "R")
-(setq ansi-color-for-comint-mode 'filter)
-(setq comint-scroll-to-bottom-on-input t)
-(setq comint-scroll-to-bottom-on-output t)
-(setq comint-move-point-for-output t)
 (setq ess-nuke-trailing-whitespace-p t)
 (ess-toggle-underscore nil)
-(setq ess-nuke-trailing-whitespace-p t)
 ;; turn off aligning single '#' to col 40!
 (setq ess-fancy-comments nil)
-
-(autoload 'R-mode "R mode" "mode for interacting with R" t)
- (setq auto-mode-alist
-       (append '(("\\.[rR]$" . R-mode)
-                 ("\\.[rR]history" . R-mode)) auto-mode-alist))
-
 ;; End setup R-mode -----------------------------------------------------------
 
 ;; ----------------------------------------------------------------------------
@@ -122,7 +174,6 @@
 ;; (load "auctex.el" nil t t)  ;; not needed when using auctex from ELPA
 ;; (require 'latex) ;; not needed when using auctex from ELPA
 
-;(define-key LaTeX-mode-map "\C-cw" 'latex-word-count))
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 (add-hook 'LaTeX-mode-hook 'turn-on-visual-line-mode)  ;; visual line wrapping
@@ -147,23 +198,8 @@
 (setq reftex-use-multiple-selection-buffers t)
 (setq reftex-plug-into-AUCTeX t)
 
-
-;;;----------------------------------------------------------------------------
-;; Setup Shell mode
-
-;; Actually display colors when programs output colored text.  Without
-;; this command, emacs prints the actual control characters.
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-(add-hook 'shell-mode-hook
-	  (function (lambda ()
-		      (setq comint-scroll-to-bottom-on-input t)
-		      (setq comint-scroll-to-bottom-on-output t)
-		      (setq comint-scroll-show-maximum-output t)
-		      (compilation-shell-minor-mode)
-		      (rename-buffer "shell" t))))
-
-;; End setup shell-mode -------------------------------------------------------
+;; setup remote file access mode for tramp ------------------------------------
+(setq tramp-default-method "ssh")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; End document modes setup
