1011c474644714c0f6b9fb4c0e28df103fd4b691
Dylan W. Schwilk <dylan@schwilk.org>
2014-10-24 08:42:24 -0500

    Remove custom count-words-region
    Emacs now has a built in version with same name that works fine.

--- lisp/efuncs.el
+++ lisp/efuncs.el
@@ -91,39 +91,6 @@ NEW-WRAP-COLUMN disables this behavior."
       (string-match "\n$" word-count)
      (message (replace-match "" nil nil word-count))))
 
-;;;----------------------------------------------------------------------------
-;; word-count-region
-(defun count-words-region (beginning end)
-  "Print number of words in the region.
-Words are defined as at least one word-constituent
-character followed by at least one character that
-is not a word-constituent.  The buffer's syntax
-table determines which characters these are."
-  (interactive "r")
-  (message "Counting words in region ... ")
-
-;;; 1. Set up appropriate conditions.
-  (save-excursion
-    (goto-char beginning)
-    (let ((count 0))
-
-;;; 2. Run the while loop.
-      (while (< (point) end)
-        (re-search-forward "\\w+\\W*")
-        (setq count (1+ count)))
-
-;;; 3. Send a message to the user.
-      (cond ((zerop count)
-             (message
-              "The region does NOT have any words."))
-            ((= 1 count)
-             (message
-              "The region has 1 word."))
-            (t
-             (message
-              "The region has %d words." count))))))
-
-
 
 ;;;----------------------------------------------------------------------------
 ;; Functions for filling and unfilling text.
