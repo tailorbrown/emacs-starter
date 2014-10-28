20e65dbe54e8a1c7b130ac186c204f0f920a85cb
Dylan W. Schwilk <dylan@schwilk.org>
2014-10-27 23:25:59 -0500

    Removed visual line mode wrap column function
    Not likely to be used by students. They can just adjust the frame size.

--- lisp/efuncs.el
+++ lisp/efuncs.el
@@ -7,57 +7,7 @@
 ;;;;---------------------------------------------------------------------------
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; check if in visual line mode before hard wrapping
-(defun maybe-fill-paragraph (&optional justify region)
-  "Fill paragraph at or after point (see `fill-paragraph'). Does
-nothing if `visual-line-mode' is on."
-  (interactive (progn
-    	 (barf-if-buffer-read-only)
-    	 (list (if current-prefix-arg 'full) t)))
-  (or visual-line-mode
-      (fill-paragraph justify region)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Set visual line mode wrap 
-(defvar visual-wrap-column nil)
-
-(defun set-visual-wrap-column (new-wrap-column &optional buffer)
-  "Force visual line wrap at NEW-WRAP-COLUMN in BUFFER (defaults
-to current buffer) by setting the right-hand margin on every
-window that displays BUFFER. A value of NIL or 0 for
-NEW-WRAP-COLUMN disables this behavior."
-  (interactive (list (read-number "New visual wrap column, 0 to disable: " (or visual-wrap-column fill-column 0))))
-  (if (and (numberp new-wrap-column)
-           (zerop new-wrap-column))
-    (setq new-wrap-column nil))
-  (with-current-buffer (or buffer (current-buffer))
-    (visual-line-mode t)
-    (set (make-local-variable 'visual-wrap-column) new-wrap-column)
-    (add-hook 'window-configuration-change-hook 'update-visual-wrap-column nil t)
-    (let ((windows (get-buffer-window-list)))
-      (while windows
-        (when (window-live-p (car windows))
-          (with-selected-window (car windows)
-            (update-visual-wrap-column)))
-        (setq windows (cdr windows))))))
-
-(defun update-visual-wrap-column ()
-  (if (not visual-wrap-column)
-    (set-window-margins nil nil)
-    (let* ((current-margins (window-margins))
-           (right-margin (or (cdr current-margins) 0))
-           (current-width (window-width))
-           (current-available (+ current-width right-margin)))
-      (if (<= current-available visual-wrap-column)
-        (set-window-margins nil (car current-margins))
-        (set-window-margins nil (car current-margins)
-                            (- current-available visual-wrap-column))))))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; My general custom functions
+;; Editing functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;----------------------------------------------------------------------------
@@ -100,8 +50,20 @@ NEW-WRAP-COLUMN disables this behavior."
 ))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Find duplicate words
+;;;----------------------------------------------------------------------------
+;;; check if in visual line mode before hard wrapping
+(defun maybe-fill-paragraph (&optional justify region)
+  "Fill paragraph at or after point (see `fill-paragraph'). Does
+nothing if `visual-line-mode' is on."
+  (interactive (progn
+    	 (barf-if-buffer-read-only)
+    	 (list (if current-prefix-arg 'full) t)))
+  (or visual-line-mode
+      (fill-paragraph justify region)))
+
+
+;;;----------------------------------------------------------------------------
+;;; Find duplicate words
 (defun the-the ()
   "Search forward for a duplicated word."
   (interactive)
@@ -113,4 +75,3 @@ NEW-WRAP-COLUMN disables this behavior."
        "\\b\\([^@ \n\t]+\\)[ \n\t]+\\1\\b" nil 'move)
       (message "Found duplicated word.")
       (message "End of buffer")))
-
